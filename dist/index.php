d0ac5d40cfdecf07db61bcc9eec0e164b0b46bc3
Sven Anders Robbestad <robbestad@gmail.com>
2014-09-25 14:42:30 +0200

    autocommit

--- dist/index.php
+++ dist/index.php
@@ -189,3 +189,13 @@ for($i=0;$i<count($data['_embedded']['robbestad']);$i++){
 <!-- Contains jQuery, React and compiled js (included jsx) -->
 <script type="text/javascript" src="./js/libs.min.js"></script>
 <script type="text/javascript" src="./js/app.min.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-45283197-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
