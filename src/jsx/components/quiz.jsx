132927c059d801e1222a3a99baf94ec9c086001f
Sven Anders Robbestad <robbestad@gmail.com>
2014-09-30 14:31:33 +0200

    autocommit

--- src/jsx/components/quiz.jsx
+++ src/jsx/components/quiz.jsx
@@ -320,10 +320,27 @@ var React = require('react'),
             return (
                 <div style={paddingTop}>
                 <section>
-                    <div className="spinner">
-                        <div className="dot1"></div>
-                        <div className="dot2"></div>
+                    <div className"spinner">
+                        <div className"spinner-container container1">
+                            <div className"circle1"></div>
+                            <div className"circle2"></div>
+                            <div className"circle3"></div>
+                            <div className"circle4"></div>
+                        </div>
+                        <div className"spinner-container container2">
+                            <div className"circle1"></div>
+                            <div className"circle2"></div>
+                            <div className"circle3"></div>
+                            <div className"circle4"></div>
+                        </div>
+                        <div className"spinner-container container3">
+                            <div className"circle1"></div>
+                            <div className"circle2"></div>
+                            <div className"circle3"></div>
+                            <div className"circle4"></div>
+                        </div>
                     </div>
+
                     <span className="quizinfo">
                     Each correct answers scores one point. Winning streaks are rewarded.
                     Additionally, completing the quiz faster earns you a time bonus.
